49eed93b95c1d844ad3567460a454ecc63949e61
unknown <slavabrodnitski@TRAINING12.BY-MINSK.CSI.local>
2012-09-04 14:13:33 +0300

    first loading

---
+++ testProject/src/main/java/com/issoft/testapp/presentation/action/WelcomeUserAction.java
@@ -0,0 +1,29 @@
+package com.issoft.testapp.presentation.action;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author slavabrodnitski
+ */
+public class WelcomeUserAction implements Action{
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // all struts logic here
+    @Override
+    public String execute() {
+        
+        return "SUCCESS";
+
+    }
+}
