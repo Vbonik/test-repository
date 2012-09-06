6d3d4454d41c2866d5bd7e2c7c6162edb49966a0
vbonik <SlavaBrodnitski@coherentsolutions.com>
2012-09-06 10:10:06 +0300

    FtpClientService

---
+++ testProject/src/main/java/com/issoft/testapp/ftp/client/FtpClientService.java
@@ -0,0 +1,122 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.issoft.testapp.ftp.client;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Properties;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPConnectionClosedException;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author slavabrodnitski
+ */
+public class FtpClientService {
+
+    // private static final String ERROR_MSG_BAD_ADDRESS = "Invalid host address.";
+    private static final Logger logger = Logger.getLogger(FtpClientService.class);
+    private final FTPClient client = new FTPClient();
+    private final InetAddress host;
+    private final Integer port;
+    private Boolean isLoggined = false;
+
+    public FtpClientService() throws UnknownHostException, FtpException {
+        this(InetAddress.getByName("localhost"), null);
+    }
+
+    public FtpClientService(InetAddress host, Integer port) throws FtpException {
+        if (host != null) {
+            this.host = host;
+            try {
+                this.port = port;
+                if (port != null) {
+                    client.connect(host, port);
+                } else {
+                    client.connect(host);
+                }
+                return;
+            } catch (IOException ioEx) {
+                logger.error("Connection failed.");
+            }
+        }
+        throw new FtpException("Ivalid address.");
+    }
+
+    public Boolean uploadFile(String remoteName, String filePath) {
+        if ((remoteName != null) && (filePath != null)) {
+            try {
+                return client.storeFile(remoteName, new FileInputStream(filePath));
+            } catch (SocketException sEx) {
+                logger.error(sEx);
+            } catch (IOException ioEx) {
+                logger.error(ioEx);
+            }
+        }
+        return false;
+    }
+
+    public File downloadFile(String remotePath, String localPath) {
+        if ((isLoggined) && (remotePath != null)) {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader
+                        (client.retrieveFileStream(remotePath)));
+                StringBuilder strBuilder = new StringBuilder();
+                while (reader.ready()) {
+                    strBuilder.append(reader.readLine());
+                }
+                File readedFile = new File(localPath);
+                OutputStream s = new FileOutputStream(readedFile);
+                s.write(strBuilder.toString().getBytes());
+                s.flush();
+                return readedFile;                
+            } catch (IOException ioEx) {
+                
+            }            
+        }
+        return null;
+    }
+
+    public Boolean login(String login, String password) {
+        if (login != null) {
+            try {
+                return isLoggined = client.login(login, password);
+            } catch (IOException ioEx) {
+                logger.error("Invalid login or password", ioEx);
+            }
+        }
+        return false;
+    }
+
+    public void close() {
+        try {
+            client.logout();
+            client.disconnect();
+        } catch (FTPConnectionClosedException fcEx) {
+        } catch (IOException ioEx) {
+        }
+    }
+
+    public Boolean isLoggined() {
+        return isLoggined;
+    }
+}
--- testProject/src/main/java/com/issoft/testapp/presentation/action/WelcomeUserAction.java
+++ testProject/src/main/java/com/issoft/testapp/presentation/action/WelcomeUserAction.java
@@ -1,7 +1,13 @@
 package com.issoft.testapp.presentation.action;
 
+import com.issoft.testapp.ftp.client.FtpClientService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+import java.io.File;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.apache.ftpserver.ftplet.FtpException;
 
 /**
  *
@@ -9,6 +15,7 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class WelcomeUserAction implements Action{
 
+    private FtpClientService ftpService;
     private String username;
 
     public String getUsername() {
@@ -21,8 +28,16 @@ public class WelcomeUserAction implements Action{
 
     // all struts logic here
     @Override
-    public String execute() {
-
+    public String execute() throws UnknownHostException{
+        try {
+            ftpService = new FtpClientService(Inet4Address.getByName("localhost"), 2121);
+            ftpService.login("admin", "admin");
+        } catch(FtpException fEx) {
+            
+        } catch(UnknownHostException uhEx) {
+            
+        }
+        File file = ftpService.downloadFile("nick.torrent", "d:/ttt.torrent");       
         return "SUCCESS";
 
     }
