ef6d6b66373ae97ad8c4b3f8a0e035b380986079
Mikita Dadydau <daff.nik@gmail.com>
2012-09-10 00:21:29 +0300

    DN commit

--- testProject/src/main/java/com/issoft/ftp/client/FtpClientService.java
+++ testProject/src/main/java/com/issoft/ftp/client/FtpClientService.java
@@ -4,46 +4,51 @@
  */
 package com.issoft.ftp.client;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.Properties;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPConnectionClosedException;
-import org.apache.commons.net.ftp.FTPFile;
 import org.apache.ftpserver.ftplet.FtpException;
 import org.apache.log4j.Logger;
 
 /**
- *
  * @author slavabrodnitski
  */
 public class FtpClientService {
 
-    private static final Integer DEFAULT_FTP_LISTENER_PORT  = 2121;
+    private static final Integer DEFAULT_FTP_LISTENER_PORT = 2121;
     private static final Logger logger = Logger.getLogger(FtpClientService.class);
-    
-    
+
+
     private final FTPClient client = new FTPClient();
     private final InetAddress host;
     private final Integer port;
-    private Boolean isLoggined = false;
+    private Boolean logged = false;
 
+    // org.apache.ftpserver.ftplet.FtpException: Ivalid address. - becouse Server is not started!
     public FtpClientService() throws UnknownHostException, FtpException {
+        //TODO: must use try catch to this ex.
         this(InetAddress.getByName("localhost"), DEFAULT_FTP_LISTENER_PORT);
+    }
+
+    public Boolean login(String login, String password) {
+        if (login != null) {
+            try {
+                return logged = client.login(login, password);
+            } catch (IOException ioEx) {
+                logger.error("Invalid login or password", ioEx);
+            }
+        }
+        return false;
     }
 
     public FtpClientService(InetAddress host, Integer port) throws FtpException {
@@ -64,10 +69,11 @@ public class FtpClientService {
         throw new FtpException("Ivalid address.");
     }
 
-    public Boolean uploadFile(String remoteName, String filePath) {
-        if ((remoteName != null) && (filePath != null)) {
+    //TODO: something bad with this stuff
+    public Boolean uploadFile(String remoteName, File file) {
+        if ((remoteName != null) && (file != null)) {
             try {
-                return client.storeFile(remoteName, new FileInputStream(filePath));
+                return client.storeFile(remoteName, new FileInputStream(file));
             } catch (SocketException sEx) {
                 logger.error(sEx);
             } catch (IOException ioEx) {
@@ -78,7 +84,7 @@ public class FtpClientService {
     }
 
     public File downloadFile(String remotePath, String localPath) {
-        if ((isLoggined) && (remotePath != null)) {
+        if ((logged) && (remotePath != null)) {
             try {
                 BufferedReader reader = new BufferedReader(new InputStreamReader
                         (client.retrieveFileStream(remotePath)));
@@ -90,24 +96,14 @@ public class FtpClientService {
                 OutputStream s = new FileOutputStream(readedFile);
                 s.write(strBuilder.toString().getBytes());
                 s.flush();
-                return readedFile;                
+                return readedFile;
             } catch (IOException ioEx) {
-                
-            }            
+
+            }
         }
         return null;
     }
 
-    public Boolean login(String login, String password) {
-        if (login != null) {
-            try {
-                return isLoggined = client.login(login, password);
-            } catch (IOException ioEx) {
-                logger.error("Invalid login or password", ioEx);
-            }
-        }
-        return false;
-    }
 
     public void close() {
         try {
@@ -118,7 +114,7 @@ public class FtpClientService {
         }
     }
 
-    public Boolean isLoggined() {
-        return isLoggined;
+    public Boolean isLogged() {
+        return logged;
     }
 }
