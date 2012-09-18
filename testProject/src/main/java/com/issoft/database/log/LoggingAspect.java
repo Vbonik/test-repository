83c0018b89cb5035f6988f8aa1ff2b2afdb0cdf3
Anton Sidorovich <cidyuk@gmail.com>
2012-09-18 14:37:33 +0300

    [A] Added logging for file download.

--- testProject/src/main/java/com/issoft/database/log/LoggingAspect.java
+++ testProject/src/main/java/com/issoft/database/log/LoggingAspect.java
@@ -4,12 +4,16 @@ import com.issoft.database.log.entry.LogEntryDAO;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+
+import java.io.InputStream;
 
 /**
  * @author: AS
@@ -17,12 +21,17 @@ import org.springframework.security.core.userdetails.User;
 @Aspect
 public class LoggingAspect {
 
-    private User user;
-    private String action;
-    private String status;
+    private String filePath;
     private LogEntryDAO logEntryDAO;
+    private static final String SPRING_SECURITY_AUTHENTICATION = "Spring security authentication";
+    private static final String SUCCESS = "SUCCESS";
+    private static final String FAILURE = "FAILURE";
 
-
+    /**
+     * Failed spring security authentication.
+     *
+     * @param joinPoint
+     */
     @After("execution(* org.springframework.security.web.authentication." +
             "SimpleUrlAuthenticationFailureHandler.onAuthenticationFailure(..))")
     public void springSecurityAuthenticationFailed(JoinPoint joinPoint) {
@@ -30,31 +39,89 @@ public class LoggingAspect {
         AuthenticationException exception = (AuthenticationException) paramValues[2];
         Authentication authentication = exception.getAuthentication();
         String principalName = (String) authentication.getPrincipal();
-        action = "Spring security authentication";
-        status = "FAILURE: " + exception.getMessage();
+        String action = SPRING_SECURITY_AUTHENTICATION;
+        String status = FAILURE + ": " + exception.getMessage();
         logEntryDAO.saveEntry(principalName, "", action, status);
     }
 
+    /**
+     * Successful spring security authentication.
+     *
+     * @param joinPoint
+     */
     @After("execution(* org.springframework.security.web.authentication." +
             "AuthenticationSuccessHandler.onAuthenticationSuccess(..))")
     public void springSecurityAuthenticationSuccess(JoinPoint joinPoint) {
         Object[] paramValues = joinPoint.getArgs();
         Authentication authentication = (Authentication) paramValues[2];
-        user = (User) authentication.getPrincipal();
-        action = "Spring security authentication";
-        status = "SUCCESS";
+        User user = (User) authentication.getPrincipal();
+        String action = SPRING_SECURITY_AUTHENTICATION;
+        String status = SUCCESS;
         logEntryDAO.saveEntry(user.getUsername(), getAuthorities(user), action, status);
     }
 
+    /**
+     * Ftp server. Login.
+     *
+     * @param joinPoint
+     * @param result
+     */
     @AfterReturning(
             pointcut = "execution(* com.issoft.ftp.presentation.action.FtpAction.login()))",
             returning = "result")
-    public void logLogin(JoinPoint joinPoint, Object result) {
-        user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        action = joinPoint.getSignature().getName();
-        status = result.toString();
+    public void loginFtp(JoinPoint joinPoint, Object result) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String action = joinPoint.getSignature().getName();
+        String status = result.toString();
         logEntryDAO.saveEntry(user.getUsername(), getAuthorities(user), action, status);
     }
+
+    /**
+     * Ftp server. Download file.
+     */
+    @Pointcut("execution(* com.issoft.ftp.client.FtpClientService.downloadFile(..)))")
+    public void download() {
+    }
+
+    /**
+     * Ftp server. Successful start download file.
+     *
+     * @param joinPoint
+     * @param inputStream
+     */
+    @AfterReturning(pointcut = "download()",
+            returning = "inputStream")
+    public void downloadStartSuccess(JoinPoint joinPoint, InputStream inputStream) {
+        filePath = (String) joinPoint.getArgs()[0];
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String action = "Starting download file: " + filePath;
+        String status = SUCCESS;
+        logEntryDAO.saveEntry(user.getUsername(), getAuthorities(user), action, status);
+    }
+
+    @AfterThrowing(pointcut = "download()",
+            throwing = "exception")
+    public void downloadStartFailed(JoinPoint joinPoint, Throwable exception) {
+        filePath = (String) joinPoint.getArgs()[0];
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String action = "Starting download file: " + filePath;
+        String status = FAILURE + ": " + exception.getMessage();
+        logEntryDAO.saveEntry(user.getUsername(), getAuthorities(user), action, status);
+    }
+
+
+    /*  @Pointcut("execution(* com.issoft.ftp.presentation.action.FtpAction.download(..)))")
+        public void downloadAction() {
+        }
+
+        @AfterReturning(pointcut = "downloadAction()",
+            returning = "result")
+    public void downloadSuccess(JoinPoint joinPoint, Object result) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String action = "Download file: " + filePath;
+        String status = result.toString();
+        logEntryDAO.saveEntry(user.getUsername(), getAuthorities(user), action, status);
+    }*/
 
     /**
      * Gets collection of user authorities and converting them to one string.
