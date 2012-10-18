9318d435b0e8cd962d9999484f48718a244bfc3e
Anton Sidorovich <cidyuk@gmail.com>
2012-10-18 11:24:33 +0300

    [A] Jenkins support

--- database_scheme.sql
+++ database_scheme.sql
@@ -3,22 +3,23 @@ CREATE TABLE `user_roles` (
   `authority` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `users` (
   `userid` varchar(64) NOT NULL,
-  `userpassword` varchar(64) DEFAULT NULL,
-  `homedirectory` varchar(128) DEFAULT NULL,
+  `userpassword` varchar(45) DEFAULT NULL,
   `enableflag` tinyint(1) DEFAULT '1',
-  `writepermission` tinyint(1) DEFAULT '0',
+  `role_id` int(11) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `homedirectory` varchar(120) DEFAULT NULL,
+  `writepermission` tinyint(1) DEFAULT '1',
   `idletime` int(11) DEFAULT '0',
   `uploadrate` int(11) DEFAULT '0',
   `downloadrate` int(11) DEFAULT '0',
   `maxloginnumber` int(11) DEFAULT '0',
   `maxloginperip` int(11) DEFAULT '0',
-  `role_id` int(11) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`userid`),
+  UNIQUE KEY `id_UNIQUE` (`userid`),
   KEY `fk_users_user_roles_idx` (`role_id`),
   CONSTRAINT `fk_users_user_roles` FOREIGN KEY (`role_id`) REFERENCES `user_roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -31,17 +32,17 @@ CREATE TABLE `log_table` (
   `status` longtext,
   `date` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `email_notifications` (
   `notification_id` int(11) NOT NULL,
   `notification` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`notification_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `users_notifications` (
-  `user_id` int(11) NOT NULL,
+  `user_id` varchar(45) NOT NULL,
   `notification_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`,`notification_id`),
   KEY `FK_NOTIFICATION` (`notification_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
